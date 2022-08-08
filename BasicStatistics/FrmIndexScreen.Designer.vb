8009148de70b8e154db035499a54c17739103f45
Mon Albert <mrgamilmonalbert@gmail.com>
2022-08-08 23:17:02 +0800

    Add project files.

---
+++ BasicStatistics/FrmIndexScreen.Designer.vb
@@ -0,0 +1,275 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmIndexScreen
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PnlValue = New System.Windows.Forms.Panel()
+        Me.TxtValue = New System.Windows.Forms.TextBox()
+        Me.PnlInput = New System.Windows.Forms.Panel()
+        Me.TxtIndex = New System.Windows.Forms.TextBox()
+        Me.BtnInput = New System.Windows.Forms.Button()
+        Me.BtnChange = New System.Windows.Forms.Button()
+        Me.BtnCompute = New System.Windows.Forms.Button()
+        Me.BtnReset = New System.Windows.Forms.Button()
+        Me.LblIndex = New System.Windows.Forms.Label()
+        Me.LblValue = New System.Windows.Forms.Label()
+        Me.LblData = New System.Windows.Forms.Label()
+        Me.PnlData = New System.Windows.Forms.Panel()
+        Me.TxtData = New System.Windows.Forms.TextBox()
+        Me.pbxNPDC = New System.Windows.Forms.PictureBox()
+        Me.BtnShowNPDC = New System.Windows.Forms.Button()
+        Me.PnlNPDC = New System.Windows.Forms.Panel()
+        Me.LblNPDC = New System.Windows.Forms.Label()
+        Me.PnlValue.SuspendLayout()
+        Me.PnlInput.SuspendLayout()
+        Me.PnlData.SuspendLayout()
+        CType(Me.pbxNPDC, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.PnlNPDC.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'PnlValue
+        '
+        Me.PnlValue.BackColor = System.Drawing.Color.FromArgb(CType(CType(225, Byte), Integer), CType(CType(225, Byte), Integer), CType(CType(225, Byte), Integer))
+        Me.PnlValue.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PnlValue.Controls.Add(Me.TxtValue)
+        Me.PnlValue.Enabled = False
+        Me.PnlValue.Location = New System.Drawing.Point(101, 43)
+        Me.PnlValue.Name = "PnlValue"
+        Me.PnlValue.Size = New System.Drawing.Size(150, 36)
+        Me.PnlValue.TabIndex = 0
+        '
+        'TxtValue
+        '
+        Me.TxtValue.BackColor = System.Drawing.Color.FromArgb(CType(CType(225, Byte), Integer), CType(CType(225, Byte), Integer), CType(CType(225, Byte), Integer))
+        Me.TxtValue.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TxtValue.ForeColor = System.Drawing.Color.Black
+        Me.TxtValue.Location = New System.Drawing.Point(10, 10)
+        Me.TxtValue.Name = "TxtValue"
+        Me.TxtValue.Size = New System.Drawing.Size(130, 16)
+        Me.TxtValue.TabIndex = 0
+        Me.TxtValue.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'PnlInput
+        '
+        Me.PnlInput.BackColor = System.Drawing.Color.FromArgb(CType(CType(225, Byte), Integer), CType(CType(225, Byte), Integer), CType(CType(225, Byte), Integer))
+        Me.PnlInput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PnlInput.Controls.Add(Me.TxtIndex)
+        Me.PnlInput.Enabled = False
+        Me.PnlInput.Location = New System.Drawing.Point(25, 43)
+        Me.PnlInput.Name = "PnlInput"
+        Me.PnlInput.Size = New System.Drawing.Size(70, 36)
+        Me.PnlInput.TabIndex = 1
+        '
+        'TxtIndex
+        '
+        Me.TxtIndex.BackColor = System.Drawing.Color.FromArgb(CType(CType(225, Byte), Integer), CType(CType(225, Byte), Integer), CType(CType(225, Byte), Integer))
+        Me.TxtIndex.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TxtIndex.ForeColor = System.Drawing.Color.Black
+        Me.TxtIndex.Location = New System.Drawing.Point(10, 10)
+        Me.TxtIndex.Name = "TxtIndex"
+        Me.TxtIndex.Size = New System.Drawing.Size(50, 16)
+        Me.TxtIndex.TabIndex = 0
+        Me.TxtIndex.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'BtnInput
+        '
+        Me.BtnInput.Location = New System.Drawing.Point(257, 43)
+        Me.BtnInput.Name = "BtnInput"
+        Me.BtnInput.Size = New System.Drawing.Size(150, 36)
+        Me.BtnInput.TabIndex = 2
+        Me.BtnInput.Text = "&Input"
+        Me.BtnInput.UseVisualStyleBackColor = True
+        '
+        'BtnChange
+        '
+        Me.BtnChange.Enabled = False
+        Me.BtnChange.Location = New System.Drawing.Point(413, 43)
+        Me.BtnChange.Name = "BtnChange"
+        Me.BtnChange.Size = New System.Drawing.Size(150, 36)
+        Me.BtnChange.TabIndex = 3
+        Me.BtnChange.Text = "C&hange"
+        Me.BtnChange.UseVisualStyleBackColor = True
+        '
+        'BtnCompute
+        '
+        Me.BtnCompute.Enabled = False
+        Me.BtnCompute.Location = New System.Drawing.Point(56, 454)
+        Me.BtnCompute.Name = "BtnCompute"
+        Me.BtnCompute.Size = New System.Drawing.Size(150, 36)
+        Me.BtnCompute.TabIndex = 4
+        Me.BtnCompute.Text = "&Compute"
+        Me.BtnCompute.UseVisualStyleBackColor = True
+        '
+        'BtnReset
+        '
+        Me.BtnReset.Enabled = False
+        Me.BtnReset.Location = New System.Drawing.Point(212, 454)
+        Me.BtnReset.Name = "BtnReset"
+        Me.BtnReset.Size = New System.Drawing.Size(150, 36)
+        Me.BtnReset.TabIndex = 5
+        Me.BtnReset.Text = "&Reset"
+        Me.BtnReset.UseVisualStyleBackColor = True
+        '
+        'LblIndex
+        '
+        Me.LblIndex.AutoSize = True
+        Me.LblIndex.Location = New System.Drawing.Point(25, 25)
+        Me.LblIndex.Name = "LblIndex"
+        Me.LblIndex.Size = New System.Drawing.Size(39, 15)
+        Me.LblIndex.TabIndex = 6
+        Me.LblIndex.Text = "Index:"
+        '
+        'LblValue
+        '
+        Me.LblValue.AutoSize = True
+        Me.LblValue.Location = New System.Drawing.Point(98, 25)
+        Me.LblValue.Name = "LblValue"
+        Me.LblValue.Size = New System.Drawing.Size(38, 15)
+        Me.LblValue.TabIndex = 7
+        Me.LblValue.Text = "Value:"
+        '
+        'LblData
+        '
+        Me.LblData.AutoSize = True
+        Me.LblData.Location = New System.Drawing.Point(25, 97)
+        Me.LblData.Name = "LblData"
+        Me.LblData.Size = New System.Drawing.Size(34, 15)
+        Me.LblData.TabIndex = 8
+        Me.LblData.Text = "Data:"
+        '
+        'PnlData
+        '
+        Me.PnlData.BackColor = System.Drawing.Color.White
+        Me.PnlData.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PnlData.Controls.Add(Me.TxtData)
+        Me.PnlData.Enabled = False
+        Me.PnlData.Location = New System.Drawing.Point(25, 115)
+        Me.PnlData.Name = "PnlData"
+        Me.PnlData.Size = New System.Drawing.Size(538, 318)
+        Me.PnlData.TabIndex = 2
+        '
+        'TxtData
+        '
+        Me.TxtData.BackColor = System.Drawing.Color.White
+        Me.TxtData.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TxtData.Location = New System.Drawing.Point(10, 10)
+        Me.TxtData.Margin = New System.Windows.Forms.Padding(10)
+        Me.TxtData.Multiline = True
+        Me.TxtData.Name = "TxtData"
+        Me.TxtData.ReadOnly = True
+        Me.TxtData.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TxtData.Size = New System.Drawing.Size(516, 296)
+        Me.TxtData.TabIndex = 0
+        '
+        'pbxNPDC
+        '
+        Me.pbxNPDC.BackColor = System.Drawing.Color.White
+        Me.pbxNPDC.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.pbxNPDC.Location = New System.Drawing.Point(588, 115)
+        Me.pbxNPDC.Name = "pbxNPDC"
+        Me.pbxNPDC.Size = New System.Drawing.Size(538, 318)
+        Me.pbxNPDC.TabIndex = 9
+        Me.pbxNPDC.TabStop = False
+        '
+        'BtnShowNPDC
+        '
+        Me.BtnShowNPDC.Enabled = False
+        Me.BtnShowNPDC.Location = New System.Drawing.Point(368, 454)
+        Me.BtnShowNPDC.Name = "BtnShowNPDC"
+        Me.BtnShowNPDC.Size = New System.Drawing.Size(150, 36)
+        Me.BtnShowNPDC.TabIndex = 10
+        Me.BtnShowNPDC.Text = "&Show NPDC"
+        Me.BtnShowNPDC.UseVisualStyleBackColor = True
+        '
+        'PnlNPDC
+        '
+        Me.PnlNPDC.BackColor = System.Drawing.Color.PaleGreen
+        Me.PnlNPDC.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PnlNPDC.Controls.Add(Me.LblNPDC)
+        Me.PnlNPDC.Location = New System.Drawing.Point(588, 43)
+        Me.PnlNPDC.Name = "PnlNPDC"
+        Me.PnlNPDC.Size = New System.Drawing.Size(538, 36)
+        Me.PnlNPDC.TabIndex = 1
+        '
+        'LblNPDC
+        '
+        Me.LblNPDC.Location = New System.Drawing.Point(133, 10)
+        Me.LblNPDC.Name = "LblNPDC"
+        Me.LblNPDC.Size = New System.Drawing.Size(272, 16)
+        Me.LblNPDC.TabIndex = 11
+        Me.LblNPDC.Text = "Normal Probability Distribution Curve (NPDC)"
+        Me.LblNPDC.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'FrmIndexScreen
+        '
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
+        Me.ClientSize = New System.Drawing.Size(1153, 514)
+        Me.Controls.Add(Me.PnlNPDC)
+        Me.Controls.Add(Me.BtnShowNPDC)
+        Me.Controls.Add(Me.pbxNPDC)
+        Me.Controls.Add(Me.PnlData)
+        Me.Controls.Add(Me.LblData)
+        Me.Controls.Add(Me.LblValue)
+        Me.Controls.Add(Me.LblIndex)
+        Me.Controls.Add(Me.BtnReset)
+        Me.Controls.Add(Me.BtnCompute)
+        Me.Controls.Add(Me.BtnChange)
+        Me.Controls.Add(Me.BtnInput)
+        Me.Controls.Add(Me.PnlInput)
+        Me.Controls.Add(Me.PnlValue)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ForeColor = System.Drawing.Color.Black
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "FrmIndexScreen"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Basic Statistics"
+        Me.PnlValue.ResumeLayout(False)
+        Me.PnlValue.PerformLayout()
+        Me.PnlInput.ResumeLayout(False)
+        Me.PnlInput.PerformLayout()
+        Me.PnlData.ResumeLayout(False)
+        Me.PnlData.PerformLayout()
+        CType(Me.pbxNPDC, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.PnlNPDC.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PnlValue As Panel
+    Friend WithEvents TxtValue As TextBox
+    Friend WithEvents PnlInput As Panel
+    Friend WithEvents TxtIndex As TextBox
+    Friend WithEvents BtnInput As Button
+    Friend WithEvents BtnChange As Button
+    Friend WithEvents BtnCompute As Button
+    Friend WithEvents BtnReset As Button
+    Friend WithEvents LblIndex As Label
+    Friend WithEvents LblValue As Label
+    Friend WithEvents LblData As Label
+    Friend WithEvents PnlData As Panel
+    Friend WithEvents TxtData As TextBox
+    Friend WithEvents pbxNPDC As PictureBox
+    Friend WithEvents BtnShowNPDC As Button
+    Friend WithEvents PnlNPDC As Panel
+    Friend WithEvents LblNPDC As Label
+End Class
---
+++ BasicStatistics/FrmIndexScreen.vb
@@ -0,0 +1,227 @@
+﻿' This code will use the addded reference "Microsoft Excel Object Model"
+Imports System.Text.RegularExpressions
+Imports Excel = Microsoft.Office.Interop.Excel
+
+Public Class FrmIndexScreen
+    Dim SngData() As Single ' This variable will hold the data to be displayed
+    Dim SngMean As Single   ' This variable will hold the value of the mean
+    Dim SngSD As Single     ' This variable will hold the value of the standard deviation
+    Dim ExportDirectoryString = My.Computer.FileSystem.SpecialDirectories.Desktop & "\Chart.gif"
+
+    Private Sub FrmIndexScreen_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub PnlValue_MouseClick(sender As Object, e As MouseEventArgs) Handles PnlValue.MouseClick
+        TxtValue.Focus()
+    End Sub
+
+    Private Sub BtnInput_Click(sender As Object, e As EventArgs) Handles BtnInput.Click
+        ' This block of code will input numbers of data and their corresponding values
+        Try
+            If IsNothing(SngData) Then
+                Dim NumberOfData = InputBox("Please specify the number(s) of data.", "Number(s) of Data")
+                If String.IsNullOrWhiteSpace(NumberOfData) Then
+                    Exit Sub
+                Else
+                    ReDim SngData(Double.Parse(NumberOfData) - 1)
+                    DisplayData()
+                    PnlValue.BackColor = Color.White
+                    TxtValue.BackColor = Color.White
+                    PnlData.Enabled = True
+                    PnlValue.Enabled = True
+                    BtnChange.Enabled = True
+                    TxtIndex.Text = 0
+                    TxtValue.Focus()
+                End If
+            Else
+                If Not String.IsNullOrWhiteSpace(TxtValue.Text) Then
+                    SngData(TxtIndex.Text) = TxtValue.Text
+                    TxtIndex.Text += 1
+                    TxtValue.Clear()
+                    TxtValue.Focus()
+                    DisplayData()
+                    If TxtIndex.Text = UBound(SngData) + 1 Then
+                        PnlValue.BackColor = Color.FromArgb(225, 225, 225)
+                        TxtValue.BackColor = Color.FromArgb(225, 225, 225)
+                        PnlValue.Enabled = False
+                        BtnInput.Enabled = False
+                        BtnCompute.Enabled = True
+                    End If
+                End If
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub BtnChange_Click(sender As Object, e As EventArgs) Handles BtnChange.Click
+        Try
+            Dim TargetIndex As ULong = InputBox("Specify from which index.", "Change Value")
+            SngData(TargetIndex) = InputBox("Change the value in index " & TargetIndex & " to:", "New Value", SngData(TargetIndex))
+            BtnCompute.Enabled = True
+            DisplayData()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+            Exit Sub
+        End Try
+    End Sub
+
+    Private Sub BtnCompute_Click(sender As Object, e As EventArgs) Handles BtnCompute.Click
+        Try
+            Dim dblSum As Double
+            For i = 0 To UBound(SngData)
+                dblSum += SngData(i)
+            Next
+            SngMean = dblSum / (TxtIndex.Text)
+            Dim dblSum2 As Double
+            For j = 0 To UBound(SngData)
+                dblSum2 += (SngData(j) - SngMean) ^ 2
+            Next
+            SngSD = Math.Sqrt(dblSum2 / TxtIndex.Text)
+
+            TxtData.Text &= vbCrLf & "MEAN = " & Math.Round(SngMean, 3) & vbCrLf _
+                                & "STANDARD DEVIATION = " & Math.Round(SngSD, 3)
+            BtnCompute.Enabled = False
+            BtnReset.Enabled = True
+            BtnShowNPDC.Enabled = True
+            SngMean = Math.Round(SngMean, 3)
+            SngSD = Math.Round(SngSD, 3)
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub BtnReset_Click(sender As Object, e As EventArgs) Handles BtnReset.Click
+        ' This block of code will return the original state of the application
+        SngData = Nothing
+        SngMean = Nothing
+        SngSD = Nothing
+        TxtIndex.Clear()
+        TxtValue.Clear()
+        TxtData.Clear()
+        PnlValue.BackColor = Color.FromArgb(225, 225, 225)
+        TxtValue.BackColor = Color.FromArgb(225, 225, 225)
+        PnlValue.Enabled = False
+        PnlData.Enabled = False
+        BtnCompute.Enabled = False
+        BtnReset.Enabled = False
+        BtnShowNPDC.Enabled = False
+        BtnInput.Enabled = True
+        If ExportDirectoryString <> Nothing Then
+            pbxNPDC.Image.Dispose()
+            pbxNPDC.Image = Nothing
+            My.Computer.FileSystem.DeleteFile(ExportDirectoryString)
+            ExportDirectoryString = Nothing
+        End If
+    End Sub
+
+    Dim ExcelObject As New Excel.Application
+    Dim ExcelObjectWorkbook As Excel.Workbook
+    Dim ExcelObjectSheet As Excel.Worksheet
+    Dim ExcelObjectChart As Excel.Chart
+
+
+    Private Sub BtnShowNPDC_Click(sender As Object, e As EventArgs) Handles BtnShowNPDC.Click
+        ' This block of code will try to show the graph and return error message once an error occured
+        Try
+            ' This code will create an instance of excel
+            Dim ExcelObjectWorkbook As Excel.Workbook
+            Dim ExcelObjectSheet As Excel.Worksheet
+            Dim ExcelObjectChart As Excel.Chart
+
+            ExcelObjectChart = Nothing
+            ExcelObjectSheet = Nothing
+            ExcelObjectWorkbook = Nothing
+
+            Dim ExcelObject As New Excel.Application With {
+                .Visible = True
+            }
+
+            ' This block of code will create an excel file and 2 sheets
+            ExcelObjectWorkbook = ExcelObject.Workbooks.Add
+            ExcelObjectSheet = ExcelObject.Sheets("Sheet1")
+            ExcelObjectChart = ExcelObject.Charts.Add
+
+            Dim IncrementValue = 0.25 * SngSD
+            Dim XMin = SngMean - 4 * (SngSD)
+            Dim XMax = SngMean + 4 * (SngSD)
+            Dim RowIndex = 0
+
+            With ExcelObjectSheet
+                .Range("A1").Value = "SD Factoring"
+                .Range("B1").Value = "Probability"
+                .Range("D1").Value = "Mean"
+                .Range("D2").Value = "SD"
+                .Range("E1").Value = SngMean
+                .Range("E2").Value = SngSD
+            End With
+
+            ' Column A
+            For XCurrent = XMin To XMax Step IncrementValue
+                ExcelObjectSheet.Cells(RowIndex + 2, 1).value = XCurrent
+                RowIndex += 1
+            Next
+
+            RowIndex = 0
+
+            ' Column B
+            For XCurrent = XMin To XMax Step IncrementValue
+                ExcelObjectSheet.Cells(RowIndex + 2, 2).formula = "=NORM.DIST(A" & (RowIndex + 2) & ",E1,E2,FALSE)"
+                RowIndex += 1
+            Next
+
+            ' Chart
+            With ExcelObjectChart
+                .ApplyCustomType(Excel.XlChartType.xlXYScatterSmoothNoMarkers)
+                .SetSourceData(Source:=ExcelObjectSheet.Range("A2:B" & RowIndex))
+                .HasLegend = False
+                .HasTitle = True
+                .ChartTitle.Text = "Normal Probability Distribution Curve"
+            End With
+
+            ' Export chart
+            ExportDirectoryString = My.Computer.FileSystem.SpecialDirectories.Desktop & "\Chart.gif"
+            ExcelObjectChart.Export(Filename:=ExportDirectoryString)
+            pbxNPDC.SizeMode = PictureBoxSizeMode.Zoom
+            pbxNPDC.Image = Image.FromFile(ExportDirectoryString)
+
+            ' This will dispose the excel instance
+            ExcelObjectWorkbook.Saved = True
+            ExcelObjectChart = Nothing
+            ExcelObjectSheet = Nothing
+            ExcelObjectWorkbook = Nothing
+            ExcelObject.Quit()
+
+            BtnChange.Enabled = False
+            BtnCompute.Enabled = False
+            BtnShowNPDC.Enabled = False
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub FrmIndexScreen_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        ' This block of code will delete the image file
+        If ExportDirectoryString <> Nothing Then
+            pbxNPDC.Image.Dispose()
+            pbxNPDC.Image = Nothing
+            My.Computer.FileSystem.DeleteFile(ExportDirectoryString)
+        End If
+    End Sub
+
+    Private Sub FrmIndexScreen_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        If e.KeyCode = Keys.Escape AndAlso BtnReset.Enabled = True Then
+            BtnReset.PerformClick()
+        End If
+    End Sub
+
+    Private Sub DisplayData()
+        Dim StrHeader As String = "INDEX " & vbTab & ":" & vbTab & "VALUES" & vbCrLf & "=========================="
+        Dim StrBody As String = ""
+        For Counter = 0 To UBound(SngData) Step 1
+            StrBody &= Counter & vbTab & vbTab & SngData(Counter) & vbCrLf
+        Next
+        TxtData.Text = StrHeader & vbNewLine & StrBody
+    End Sub
+End Class
